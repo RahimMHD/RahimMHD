75a9038449468317dd1e81a7128d2d67641ffdf5
RahimMHD <100218482+RahimMHD@users.noreply.github.com>
2023-05-21 09:12:35 +0100

    hidden element

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Scroll from "react-scroll"
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import Scroll from "react-scroll"
 import './App.css'
 import LeftBar from './component/sideBarLeft'
 import RightBar from './component/sideBarRight'
@@ -10,14 +10,14 @@ import MainSection from "./component/MainSection"
 function App() {
   const [state, setState] = React.useState(true);
   document.body.style.backgroundColor = state ? "#eee" : "#343333";
-  
+
   return (
     <div className="App" style={{
       backgroundColor: state ? "#eee" : "#222"
     }}>
-       <LeftBar stt={state} change={() => setState(st => !st)}/>
-       <MainSection stt={state}/>
-       <RightBar stt={state} change={() => setState(st => !st)}/>
+      <LeftBar stt={state} change={() => setState(st => !st)} />
+      <MainSection stt={state} />
+      <RightBar stt={state} change={() => setState(st => !st)} />
     </div>
   )
 }
